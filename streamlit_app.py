c286632439ff086e473b55f413ae5afe41cd00d8
robertobru <roberto.bruschi@gmail.com>
2025-01-25 01:45:41 +0000

    fornace rimossa

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,6 @@ coordinates = [
                            (-2,13),(1,13),(4,12),(7,12),(10,12),(13,12),(16,12),
                    (-5,11),(-2,10),(1,10), (4,9), (7,9), (10,9), (13,9), (16,9),
             (-8,8),(-5,8), (-2,7), (1,7),  (4,6), (7,6), (10,6), (13,6), (16,6),
-
           (-8, 5), (-5, 5), (-2, 4), (1, 4), (4, 3), (7, 3), (10, 3), (13, 3), (16, 3),
            (-11, 2), (-8, 2), (-5, 2),                  (4, 0), (7, 0), (10, 0), (13, 0), (16, 0),
 (-14, -1), (-11, -1), (-8, -1), (-5, -1),                               (10, -3), (13, -3), (16, -3),
@@ -24,6 +23,12 @@ coordinates = [
 (-15, -13), (-12, -13), (-9, -13), (-6, -13), (-3, -13),(0, -13),(3, -13), (6, -13), (9, -13), (12, -13), (15, -13),
             (-12, -16), (-9, -16), (-6, -16), (-3, -16),(0, -16),(3, -16), (6, -16), (9, -16), (12, -16), (15, -16)
 ]
+
+coordinates = []
+for x in range(-18, 18, 3):
+    for y in range(-18, 18, 3):
+        if not((x > -6) and (x < 3) and (y > -3) and (y < 6)):
+            coordinates.append((x,y))
 
 
 buffer = BytesIO()
@@ -45,6 +50,8 @@ class Coordinate:
 
     @classmethod
     def from_str(cls, input):
+        if not input:
+            return 
         x, y = map(int, input.split(':'))
         return cls(x=x, y=y)
     
@@ -89,11 +96,12 @@ def get_nearest_cells(center: Coordinate, grid_size: int) -> List[Tuple[float, C
 def assign_cells_to_members(members: pd.DataFrame, center: Coordinate, grid_size: int) -> List[Tuple[str, Coordinate]]:
     cells = []
     for coo in coordinates:
-        if "{}".format(lato_fornace) == "sinistra":
-            current_cell = Coordinate(x=coo[0] - 5 + center.x, y=coo[1] +5 + center.y) 
-        else:
-            current_cell = Coordinate(x=coo[0] + 5 + center.x, y=coo[1] +5 + center.y)
+        # if "{}".format(lato_fornace) == "sinistra":
+        #    current_cell = Coordinate(x=coo[0] - 5 + center.x, y=coo[1] +5 + center.y) 
+        #else:
+        #    current_cell = Coordinate(x=coo[0] + 5 + center.x, y=coo[1] +5 + center.y)
         
+        current_cell = Coordinate(x=coo[0] + center.x +1, y=coo[1] + center.y - 1)
         cells.append((current_cell.get_distance_from_coordinate(center), current_cell))
         cells.sort(key=lambda x: x[0])  # Ordina per distanza dal centro
     assigned_cells = []
@@ -107,29 +115,33 @@ def assign_cells_to_members(members: pd.DataFrame, center: Coordinate, grid_size
 
 def create_grid(df: pd.DataFrame, center: Coordinate):
     board_size = 19
-    if "{}".format(lato_fornace) == "sinistra":
-        x_range = (center.x - board_size -5, center.x + board_size -5)
-    else:
-        x_range = (center.x - board_size + 5, center.x + board_size + 5)
+    # if "{}".format(lato_fornace) == "sinistra":
+    #    x_range = (center.x - board_size -5, center.x + board_size -5)
+    #else:
+    #    x_range = (center.x - board_size + 5, center.x + board_size + 5)
+    
+    x_range = (center.x - board_size, center.x + board_size)
     # Creazione della scacchiera
     fig, ax = plt.subplots()
-    for y in range(center.y - board_size +4 , center.y + board_size + 5):
+    for y in range(center.y - board_size , center.y + board_size):
         ax.axhline(y, color='black', linewidth=0.5)  # Linee orizzontali
     for x in range(*x_range):
         ax.axvline(x, color='black', linewidth=0.5)  # Linee verticali
     for index, row in df.iterrows():
+        # st.write(row["Nickname"] + " ---> " + str(row["Nuove Coordinate"]))
         coord = Coordinate.from_str(row["Nuove Coordinate"])
         ax.add_patch(plt.Rectangle((coord.x, coord.y), 1, 1, color='red', alpha=0.6))  # Riempi la cella con un colore 
         ax.text(coord.x + 0.5, coord.y + 0.5, row["Nickname"], color='black', ha='center', va='center', fontsize=4)  
     
     ax.add_patch(plt.Rectangle((center.x, center.y), 1, 1, color='violet', alpha=0.6))  # Riempi la cella con un colore 
     ax.text(center.x + 0.5, center.y + 0.5, "Marshall", color='black', ha='center', va='center', fontsize=4)
-    if "{}".format(lato_fornace) == "sinistra":
-        ax.add_patch(plt.Rectangle((center.x - 5, center.y + 5), 1, 1, color='orange', alpha=0.6))  # Riempi la cella con un colore 
-        ax.text(center.x - 4.5, center.y + 5.5, "Furnace", color='black', ha='center', va='center', fontsize=4)  
-    else:
-        ax.add_patch(plt.Rectangle((center.x + 5, center.y + 5), 1, 1, color='orange', alpha=0.6))  # Riempi la cella con un colore 
-        ax.text(center.x + 5.5, center.y + 5.5, "Furnace", color='black', ha='center', va='center', fontsize=4)  
+    #if "{}".format(lato_fornace) == "sinistra":
+    #    ax.add_patch(plt.Rectangle((center.x - 5, center.y + 5), 1, 1, color='orange', alpha=0.6))  # Riempi la cella con un colore 
+    #    ax.text(center.x - 4.5, center.y + 5.5, "Furnace", color='black', ha='center', va='center', fontsize=4)  
+    #else:
+    #    ax.add_patch(plt.Rectangle((center.x + 5, center.y + 5), 1, 1, color='orange', alpha=0.6))  # Riempi la cella con un colore 
+    #    ax.text(center.x + 5.5, center.y + 5.5, "Furnace", color='black', ha='center', va='center', fontsize=4)  
+    
     return fig
 
 
@@ -175,6 +187,7 @@ if uploadedFile:
     # available_pos = [quadrato_concentrico(Coordinate.from_str(mare_coo), step_distanza * i) for i in range(1,6)]
     members_data['Ruolo'] = members_data['Ruolo'].str.upper()
     members_data['Ruolo'].fillna("R1", inplace=True)
+    members_data['Ruolo'] = members_data['Ruolo'].str.strip()
     members_data['category_order'] = members_data['Ruolo'].apply(lambda x: categories.index(x))
 
     ordered_members = members_data.sort_values(by=['category_order', 'Potenza'], ascending=[True, False])
